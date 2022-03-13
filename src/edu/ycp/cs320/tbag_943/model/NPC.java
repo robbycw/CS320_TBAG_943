afc3abeb57d22af6a43ae2381cb0f54fc9c9d84a
cogden1 <cogden1@ycp.edu>
2022-03-13 12:41:17 -0400

    Speech, Stat, NPC classes
    Added Speech, Stat, NPC classes and methods

---
+++ src/edu/ycp/cs320/tbag_943/model/NPC.java
@@ -0,0 +1,37 @@
+package edu.ycp.cs320.tbag_943.model;
+
+import java.util.ArrayList;
+
+public class NPC {
+
+	private String name; 
+	private String[] attackMoves;
+	private int health;
+	private boolean combat;
+	
+	public NPC(String name, int health, boolean combat) {
+		this.health = health;
+		this.combat = combat;
+		this.name = name;
+	}
+	
+	public String getAttackMove(int get) {
+		
+		return attackMoves[get];
+		
+	}
+	
+	public void setAttackMove(int set, String attackAdd) {
+		
+		attackMoves[set] = attackAdd;
+		
+	}
+	
+	public boolean isAlive(int get) {
+		if(health > 0) {
+		return true;}
+		else {return false;}
+		
+	}
+	
+}
