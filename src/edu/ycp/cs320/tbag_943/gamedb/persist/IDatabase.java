2eae811e2b6151d080f8d441299628a123297f65
robbycw <robbycw9@gmail.com>
2022-04-15 17:39:34 -0400

    Comments to IDatabase
    Added comments regarding the queries needed for the database, especially on how to implement them given that the DB will save multiple games

--- src/edu/ycp/cs320/tbag_943/gamedb/persist/IDatabase.java
+++ src/edu/ycp/cs320/tbag_943/gamedb/persist/IDatabase.java
@@ -9,6 +9,8 @@ import edu.ycp.cs320.tbag_943.classes.*;
 public interface IDatabase {
 	
 	// Finds
+	public User findUserByUsernameAndPassword(String username, String password); 
+	public List<Game> findGamesByUsername(String username); 
 	public List<NPC> findNPCsByLocationID(int locationID); 
 	public List<NPC> findNPCsByCombatID(int combatID);
 	public List<Speech> findSpeechByNPCID(int npcID); 
@@ -33,20 +35,45 @@ public interface IDatabase {
 	public Integer insertGameIntoGameTable(Game game); 
 	public Integer insertPlayerIDAndItemIDIntoInventoryTable(int playerID, int itemID);
 	
+	// When we go to implement insertNewGame in the SQL DB, remember that
+	// creating a new game will require reading the CSVs and properly 
+	// adding the Game onto the existing DB without overwriting existing data. 
+	// This insert will probably replace insertGameIntoGameTable. 
+	//
+	// We need to consider how to use the auto-generation of IDs properly, 
+	// as if the first game's ids for NPCs go from 1 to n, then the second
+	// game's ids for NPCs will go from n+1 to 2n and all of these IDs need 
+	// to be properly mapped in the junction table. There should be a mathe-
+	// matical relationship that we can use given the current highest game_id. 
+	// Perhaps just by fetching the number of rows in the UserToGame table?
+	// Need # of rows in each subtable as well, starting the ID counts from 
+	// (# of rows in UserToGame) * (# of rows in a given table) + 1. 
+	public Integer insertNewGame(User user, Player player); 
+	
 	
 	// Updates
-	// Player update will need to include update to Inventory Table
+	
+	// We could get away with only implementing updateGame, as Game
+	// already contains all of these classes. However, we need to 
+	// keep in mind that doing so will require the same queries that
+	// we would use in the other updates, as we need to change each 
+	// table directly, not just the Game table. We could probably 
+	// just have updateGame call all update methods after they are all
+	// implemented. 
+	
+	// updatePlayer will need to include update to Inventory Table.
+	public boolean updateGame(Game game);
+	public boolean updateGameLog(ArrayList<String> log); 
 	public boolean updatePlayer(Player player); 
 	public boolean updateNPCs(List<NPC> npcs); 
 	public boolean updateCombat(Combat combat);
-	public boolean updatePuzzle(Puzzle puzzle); 
-	public boolean updateGameLog(ArrayList<String> log); 
-	public boolean updateGame(Game game); 
+	public boolean updatePuzzle(Puzzle puzzle);  
 	public boolean updateLoot(Loot loot); 
 	
 	
 	// Removals
 	public boolean removeGameByGameID(int gameID); 
+	public boolean removeItemFromInventoryByItemIDAndPlayerID(int itemID, int playerID); 
 	
 	
 	/* Library Example: 
