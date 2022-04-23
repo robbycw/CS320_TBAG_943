7a2f2f2adb7775f8e00c123ee139835fc5541974
JordanK1ng <jking37@ycp.edu>
2022-04-23 14:35:48 -0400

    Fixing the query statements
    Had to revise Query statements due to misunderstanding

--- src/edu/ycp/cs320/tbag_943/classes/NPC.java
+++ src/edu/ycp/cs320/tbag_943/classes/NPC.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class NPC implements Comparable {
 
 	private String name; 
-	private String[] attackMoves;
+	private ArrayList<String> attackMoves;
 	private boolean combat;
 	private HashMap<String, Stat> stats; 
 	private Item weapon; 
@@ -131,6 +131,9 @@ public class NPC implements Comparable {
 	public void setCanIntimidate(boolean bool) {
 		canIntimidate = bool;
 	}
+	public boolean getCanIntimidate() {
+		return canIntimidate;
+	}
 	
 	public boolean canPersuade() {
 		return canPersuade;
@@ -138,6 +141,10 @@ public class NPC implements Comparable {
 
 	public void setCanPersuade(boolean bool) {
 		canPersuade = bool;
+	}
+	
+	public boolean getCanPersuade() {
+		return canPersuade;
 	}
 	
 	public int getPersuasionThreshold() {
@@ -149,13 +156,13 @@ public class NPC implements Comparable {
 	}
 
 	public String getAttackMove(int get) {
-		return attackMoves[get];
+		return attackMoves.get(get);
 		
 	}
 	
-	public void setAttackMove(int set, String attackAdd) {
+	public void setAttackMove(String attackAdd) {
 		
-		attackMoves[set] = attackAdd;
+		attackMoves.add(attackAdd);
 		
 	}
 	
@@ -171,6 +178,10 @@ public class NPC implements Comparable {
 	
 	public HashMap<String, Stat> getStats(){
 		return stats; 
+	}
+	
+	public ArrayList<String> getAttacks() {
+		return attackMoves;
 	}
 	
 	public void setStats(HashMap<String, Stat> stats) {
